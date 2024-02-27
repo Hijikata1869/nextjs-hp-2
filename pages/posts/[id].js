f340748bd5d42018ff5f491e1b25f90c052afbe2
Hijikata1869 <liv.knk@gmail.com>
2024-02-27 19:18:35 +0900

    個別のブログページを作成

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,45 @@
 import Link from "next/link";
 import Layout from "../../components/Layout";
-import { getAllPostsData, getAllPostsIds } from "../../lib/posts";
+import { getPostData, getAllPostsIds } from "../../lib/posts";
+
+export default function Post({ post }) {
+  if (!post) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Layout title={post.title}>
+      <p className="m-4">
+        {"ID: "}
+        {post.id}
+      </p>
+      <p className="mb-8 text-xl font-bold">{post.title}</p>
+      <p className="px-10">{post.body}</p>
+
+      <Link href="/blog-page">
+        <div className="flex cursor-pointer mt-12">
+          <svg
+            className="w-6 h-6 mr-3"
+            dataSlot="icon"
+            fill="none"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5"
+            />
+          </svg>
+          <span>Back to blog-page</span>
+        </div>
+      </Link>
+    </Layout>
+  );
+}
 
 export async function getStaticPaths() {
   const paths = await getAllPostsIds();
